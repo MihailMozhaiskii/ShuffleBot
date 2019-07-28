0fb8b423f5696005cc628abb2521d75b97f8d957
MykhailoMozhaiskyi <mmozhaiskyi@cogniance.com>
2019-07-28 13:45:06 +0300

    Init project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:ShuffleBot/ShuffleBot.dart' as ShuffleBot;
+
+main(List<String> arguments) {
+  print('Hello world: ${ShuffleBot.calculate()}!');
+}
---
+++ test/ShuffleBot_test.dart
@@ -0,0 +1,8 @@
+import 'package:ShuffleBot/ShuffleBot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
