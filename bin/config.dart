64cdd2a0c1af5dfeb155f4c7283b8f9150312347
MykhailoMozhaiskyi <mmozhaiskyi@cogniance.com>
2019-07-29 22:52:24 +0300

    r

--- bin/config.dart
+++
@@ -1,9 +0,0 @@
-const String TELEGRAM_TOKEN = '942169461:AAEAsMYb_fF3Ki9YcuJszWB3zqz0AswbK00';
-
-const String FIREBASE_API_KEY = 'AIzaSyDt4jd7mHeUxOkXM6prWKOtJYs2pDut7KA';
-const String FIREBASE_AUTH_DOMAIN = 'chu-wa-chi.firebaseapp.com';
-const String FIREBASE_DATABASE_URL = 'https://chu-wa-chi.firebaseio.com/';
-const String FIREBASE_PROJECT_ID = 'chu-wa-chi';
-const String FIREBASE_MESSAGING_SENDER_ID = '373156631611';
-const String FIREBASE_STORAGE_BUCKET = 'chu-wa-chi.appspot.com';
-const String FIREBASE_APP_ID = '1:373156631611:web:a17d883f6d4af4c4';
